b6ea3639cae20ff666880fa100fa56b566f5d9d7
Vitalii Chyrka <viavital@ukr.net>
2022-04-19 22:53:57 +0300

    PostRequest done

---
+++ ChessGameWithFogOfWar/ChessGameWithFogOfWar/Model/Player.cs
@@ -0,0 +1,32 @@
+﻿namespace ChessGameWithFogOfWar.Model
+{
+    public class ReceivedPostData
+    {
+        public Player Player { get; set; }
+        public PlayersColor PlayersColor { get; set; }
+    }
+    public class Player
+    {
+        public Player()
+        {
+            Guid _guid = Guid.NewGuid();
+            Id = _guid;
+        }
+        public Guid Id { get; set; }
+        public string Name { get; set; }        
+    }
+    public class PlayersColor
+    {
+        public string Color { get; set; }
+
+        public ColorOfTeamEnum ReturnColorEnum()
+        {
+            if (Color == "White" || Color == "white" )
+            {
+                return ColorOfTeamEnum.White;
+            }
+            return ColorOfTeamEnum.Black;
+        }
+
+    }
+}
